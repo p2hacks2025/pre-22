8e91d75904f92e043c79e8feaf47aa75aa12f0c9
tanakarinna <abomasnow0460@gmail.com>
2025-12-16 21:00:25 +0900

    HealthKitの認証機能を実装

--- PrismStep/StepCountManager.swift
+++ PrismStep/StepCountManager.swift
@@ -7,3 +7,31 @@
 
 import Foundation
 import HealthKit//ヘルスケアのデータを使うためにインポート
+import Combine
+
+class StepCountManager: ObservableObject {
+    let healthStore = HKHealthStore()//データをアクセスするためのAppleが用意した道具
+    
+    // 取得した歩数（画面側から監視できるように @Published をつける）
+    //歩数は変わるから、変数であるvarを使う
+    @Published var todaySteps: Int = 0
+    //initはClassが呼び出された時に１回だけ実行
+    init() {
+        requestAuthorization()//許可を求める
+    }
+    func requestAuthorization() {
+        //.stepCountを使うことで歩数専用の型情報を取得し、「HKQuantityType」というクラスに渡してる。.stepCountはシステム定義値なので絶対に値があるという宣言を意味する!をつける
+        let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let readTypes: Set = [stepType]//APIに渡すために、データの型を集合（Set）に変換している
+        //toShareは書き込み権限、readは読み込み権限で：の後のやつをそれぞれ渡してる（）
+        healthStore.requestAuthorization(toShare: nil, read: readTypes) { success, error in
+            if success {
+                print("許可されました")
+                // クラス内メソッドの呼び出し
+                //self.fetchTodaySteps()
+            } else {
+                print("エラーが起きました: \(String(describing: error))")
+            }
+        }
+    }
+}
