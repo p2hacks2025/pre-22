f610623eb9c009fe47d94d620afa0c32fe4ecf7e
tanakarinna <abomasnow0460@gmail.com>
2025-12-16 22:46:13 +0900

    エラー文を追加。クエリ実行コードを追加。
    クエリエラーが発生した際にエラー分が出るようにした。
    データが見つからない時にもエラー分がでてくるようにした。
    クエリを実行するコードを書き忘れていたので、書いてただ行くデータを取得できるようにした

--- PrismStep/StepCountManager.swift
+++ PrismStep/StepCountManager.swift
@@ -39,23 +39,32 @@ class StepCountManager: ObservableObject {
         //検索対象を歩数に限定
         let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         //「今日の0時0分」から「今」までの期間を設定
-                let now = Date()//現在時刻
-                let startOfDay = Calendar.current.startOfDay(for: now)
-                let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        let now = Date()//現在時刻
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
         //データを集計するクエリを作成
         //クエリはたくさんあるヘルスケアの情報から、歩数と今日の情報だよってことを検索するもの
         let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, error in
+
+            // エラーチェック
+            if let error = error {
+                print("クエリエラー発生: \(error.localizedDescription)")
+                return
+            }
             // 結果を取り出す作業
             //guardはもし条件を満たさなかったら、ここで終了するよってもの
-            guard let result = result, let sum = result.sumQuantity() else { return }
+            guard let result = result, let sum = result.sumQuantity() else {
+                print("データが見つかりませんでした（または0歩です）")
+                return }
             let steps = Int(sum.doubleValue(for: HKUnit.count()))//歩数をintに変換して使いやすくする
             // コンソールに出力（ロジックが実装できているかの確認用）
             print("今日の歩数: \(steps)歩")
             // 取得した数図をアプリの画面に反映させる。UIの更新はメインスレッドで行うというiOSの鉄則を守るためのコード
             DispatchQueue.main.async {
                 self.todaySteps = steps//ここで値を入れてる
-                        }
+            }
         }
-            
+        //クエリを実行する
+        healthStore.execute(query)
     }
 }
