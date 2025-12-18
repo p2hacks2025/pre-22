e4b87727144f6ddd1273038e2bb6a5e90c912899
tanakarinna <abomasnow0460@gmail.com>
2025-12-18 21:33:07 +0900

    Flower3DViewというファイルを作成

---
+++ PrismStep/Flower3DView.swift
@@ -0,0 +1,18 @@
+//
+//  Flower3DView.swift
+//  PrismStep
+//
+//  Created by 田中琳菜 on 2025/12/18.
+//
+
+import SwiftUI
+
+struct Flower3DView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Flower3DView()
+}
