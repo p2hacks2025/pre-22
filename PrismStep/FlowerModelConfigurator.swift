852e084fcd56eed51a0fd0a6159c2ecb7274ce8b
tanakarinna <abomasnow0460@gmail.com>
2025-12-19 14:14:12 +0900

    花2と花3の大きさや位置を標準で使いやすい様に調節。花1はまだ調節中。 ２個前のコミットで入れ忘れたので追加。

--- PrismStep/FlowerModelConfigurator.swift
+++ PrismStep/FlowerModelConfigurator.swift
@@ -5,4 +5,36 @@
 //  Created by 田中琳菜 on 2025/12/18.
 //
 
-import Foundation
+import RealityKit
+
+// モデルごとの「個体差」を吸収して、扱いやすい標準サイズに整える係
+struct FlowerModelConfigurator {
+    
+    // 1. 設定を行うメインの関数
+    static func configure(model: Entity, name: String) {
+        
+        // 影の設定
+        model.components.set(GroundingShadowComponent(castsShadow: true))
+        
+        switch name {
+        case "Flower1":
+            // --- Flower1 (蕾) の調整 ---
+            model.scale = SIMD3<Float>(0.093, 0.093, 0.093) // 標準サイズ
+            model.position = SIMD3<Float>(0, -0.1, 0)      // 中心位置
+            
+        case "Flower2":
+            // --- Flower2 (小花) の調整 ---
+            model.scale = SIMD3<Float>(0.002, 0.002, 0.002)
+            model.position = SIMD3<Float>(0, -0.5, 0)
+            
+        case "Flower3":
+            // --- Flower3 (大花) の調整 ---
+            model.scale = SIMD3<Float>(0.005, 0.005, 0.005)
+            model.position = SIMD3<Float>(0, -0.005, 0)
+            
+        default:
+            // 想定外のモデルが来た時用
+            model.scale = SIMD3<Float>(0.1, 0.1, 0.1)
+        }
+    }
+}
